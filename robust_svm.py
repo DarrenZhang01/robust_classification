715972d0661529c480d30b9163c440ff36b84acb
DarrenZhang01 <zhibo.zhang@mail.utoronto.ca>
2020-11-14 14:55:09 -0500

    Construct a Gurobi optimization model to solve the robust SVM problem. Current problems: 1. Number of constraints is too much, and it takes long time to process; 2. I have not found how to use the square root or the general exponential term in Gurobi, which makes the L2 norm calculation incomplete.

--- robust_svm.py
+++ robust_svm.py
@@ -10,10 +10,39 @@ Reference: Bertsimas, D., Dunn, J., Pawlowski, C., & Zhuo, Y. D. (2019).
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from gurobipy import *
 
 (X_train, Y_train), (X_test, Y_test) = keras.datasets.mnist.load_data(path="mnist.npz")
 
 print("X_train: {}".format(X_train.shape))
 print("Y_train: {}".format(Y_train.shape))
 print("X_test: {}".format(X_test.shape))
-print("Y_test: {}".format(Y_test.shape))
+print("Y_test: {}".format(Y_test))
+# The shape of the data:
+# X_train: (60000, 28, 28)
+# Y_train: (60000,)
+# X_test: (10000, 28, 28)
+# Y_test: (10000,)
+
+X_train = np.reshape(X_train, (60000, 784))
+X_test = np.reshape(X_test, (10000, 784))
+# Binarize label y such that it is equal to 1 when the digit is 1 and it is -1
+# when the digit belongs to other classes.
+Y_train_bin = np.where(Y_train < 1.5, 1, -1)
+Y_test_bin = np.where(Y_test < 1.5, 1, -1)
+
+
+NUM_DATA = 60000
+NUM_FEATURES = 784
+
+SVM = Model("robust_svm")
+
+itas = SVM.addVars(range(NUM_DATA), vtype=GRB.CONTINUOUS, obj=[1]*NUM_DATA)
+W = SVM.addVars(range(NUM_FEATURES), vtype=GRB.CONTINUOUS, obj=[0]*NUM_FEATURES)
+b = SVM.addVar(vtype=GRB.CONTINUOUS, obj=0)
+
+SVM.modelSense = GRB.MINIMIZE
+
+SVM.addConstrs([Y_train[i] * (sum([W[j] * X_train[i][j] for j in range(NUM_FEATURES)]) - b) - sum([W[k] * W[k] for k in range(NUM_FEATURES)]) >= 1 - itas[i] for i in range(NUM_DATA)])
+
+SVM.optimize()
