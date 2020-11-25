5f9e3e26e03620b10e32ce7bb4fc2c675691b6f5
DarrenZhang01 <zhibo.zhang@mail.utoronto.ca>
2020-11-25 16:20:02 -0500

    Train a general SVM as the comparison.

--- robust_svm.py
+++ robust_svm.py
@@ -12,6 +12,8 @@ import tensorflow as tf
 from tensorflow import keras
 from sklearn import datasets
 from sklearn.metrics import hinge_loss
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 from gurobipy import *
 #
 # (X_train, Y_train), (X_test, Y_test) = keras.datasets.mnist.load_data(path="mnist.npz")
@@ -37,12 +39,18 @@ iris = datasets.load_iris()
 X = iris.data
 Y = np.where(iris.target < 1.5, 1, -1)
 
-print(X.shape)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
+
+
+print(X_train.shape)
 print(Y)
 
 
-NUM_DATA = 150
+NUM_DATA = 105
 NUM_FEATURES = 4
+
+
+##################### Use Gurobi to train a Robust SVM ########################
 
 SVM = Model("robust_svm")
 
@@ -53,7 +61,7 @@ b = SVM.addVar(vtype=GRB.CONTINUOUS, obj=0)
 SVM.modelSense = GRB.MINIMIZE
 
 for i in range(NUM_DATA):
-    SVM.addConstr(Y[i] * (quicksum([W[j] * X[i][j] for j in \
+    SVM.addConstr(Y_train[i] * (quicksum([W[j] * X_train[i][j] for j in \
                   range(NUM_FEATURES)]) - b) - quicksum([W[k] * W[k] for k in \
                   range(NUM_FEATURES)]) >= 1 - itas[i])
 
@@ -63,7 +71,17 @@ print("W: {}".format(W))
 print("b: {}".format(b))
 
 W_np = np.reshape(np.array([W[0].x, W[1].x, W[2].x, W[3].x]), (4, 1))
-Y_pred = X @ W_np + b.x
+Y_pred = X_test @ W_np + b.x
 
-loss = hinge_loss(Y, Y_pred)
-print("the overall training loss is: {}".format(loss))
+loss = hinge_loss(Y_test, Y_pred)
+print("the overall test loss for Robust SVM: {}".format(loss))
+
+##################### Train a general Support Vector Machine ###################
+
+SVC = SVC()
+SVC.fit(X_train, Y_train)
+
+Y_pred_SVC = SVC.predict(X_test)
+SVC_loss = hinge_loss(Y_test, Y_pred_SVC)
+
+print("the overall test loss for the general SVM classifier: {}".format(SVC_loss))
