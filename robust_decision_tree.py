884e0f3f80ca78d91a02b94c4356d3693fe2f597
Zhibo (Darren) Zhang <zhibo.zhang@mail.utoronto.ca>
2020-12-07 16:35:10 -0500

    Add some degree of feature robustness according to Bertsimas, D., Dunn, J., Pawlowski, C., & Zhuo, Y. D. (2019). Robust classification. INFORMS Journal on Optimization, 1(1), 2-34.

--- robust_decision_tree.py
+++ robust_decision_tree.py
@@ -27,9 +27,10 @@ NUM_DATA = 105
 # Specify the number of nodes in the tree.
 K = 7
 # Specify the coefficients lambda for the variable d.
-lambda_ = [0.01] * K
+lambda_ = [1] * K
 N = 10
 epsilon = 0.01
+rho = 0.05
 
 def find_all_parents(node):
     cur = node
@@ -99,10 +100,10 @@ model.addConstrs(Z.sum('*', k) >= N * C[k] for k in range(K))
 # model.addConstrs(C[k] == D[k] for k in range(K))
 for k in range(K):
     model.addConstr(C[k] >= D[k] - quicksum([D[j] for j in (find_all_parents(k)[0] + find_all_parents(k)[1])]))
-model.addConstrs(quicksum([A[j, f] * X_train[i][f] for f in range(4)]) + \
+model.addConstrs(quicksum([A[j, f] * X_train[i][f] for f in range(4)]) + rho +\
                 epsilon <= B[j] + NUM_DATA * (1 - Z[i, k]) for i in range(NUM_DATA) \
                 for k in range(K) for j in find_all_parents(k)[0])
-model.addConstrs(quicksum([A[j, f] * X_train[i][f] for f in range(4)]) >= \
+model.addConstrs(quicksum([A[j, f] * X_train[i][f] for f in range(4)]) - rho >= \
                 B[j] - NUM_DATA * (1 - Z[i, k]) for i in range(NUM_DATA) \
                 for k in range(K) for j in find_all_parents(k)[1])
 
